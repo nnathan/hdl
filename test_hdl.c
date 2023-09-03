54d85755549c52cda726039d76af80e115911f27
Naveen Nathan <naveen@lastninja.net>
2023-09-04 02:12:18 +1000

    More unit tests

--- test_hdl.c
+++ test_hdl.c
@@ -311,6 +311,44 @@ int main(int argc, char **argv) {
         assert(i == el_n);
         free(el);
     }
+    // --- (0,2) (0,2) (2,1)F
+    {
+        struct hole_descriptor_list *hdl;
+        hole_descriptor_list_init(&hdl);
+        assert(hdl != NULL);
+        const int n = 3;
+        struct fragment *l = calloc(n, sizeof(struct fragment));
+        l[0].offset = 0;
+        l[0].len = 2;
+        l[1].offset = 0;
+        l[1].len = 2;
+        l[2].offset = 2;
+        l[2].len = 1;
+        l[2].final = true;
+
+        PRINT_FRAGS(l, n);
+
+        for (int i=0; i<n; i++) {
+            hole_descriptor_list_add(hdl, l[i].offset, l[i].len, l[i].final, NULL);
+        }
+
+        free(l);
+
+        const int el_n = 2;
+        struct fragment *el = calloc(el_n, sizeof(struct fragment));
+        el[0].offset = 0;
+        el[0].len = 2;
+        el[1].offset = 2;
+        el[1].len = 1;
+
+        int i = 0;
+        for (struct frag_list *fl = hdl->frag_head; fl; fl = fl->next, i++) {
+            assert(fl->offset == el[i].offset);
+            assert(fl->len == el[i].len);
+        }
+        assert(i == el_n);
+        free(el);
+    }
 
     return 0;
 }
